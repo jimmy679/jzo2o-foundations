ef400f03b408e315f81b1e99006ce2c58a2b6567
jimmy679 <1037892796@qq.com>
2024-06-23 15:48:26 +0800

    运营端 - 区域服务相关接口：启用、禁用区域、

--- src/main/java/com/jzo2o/foundations/mapper/RegionMapper.java
+++ src/main/java/com/jzo2o/foundations/mapper/RegionMapper.java
@@ -3,6 +3,7 @@ package com.jzo2o.foundations.mapper;
 import com.jzo2o.foundations.model.domain.Region;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * <p>
@@ -13,4 +14,5 @@ import org.apache.ibatis.annotations.Mapper;
  * @since 2023-07-03
  */
 public interface RegionMapper extends BaseMapper<Region> {
+    Integer queryByRegionId(@Param("regionId") Long regionId);
 }
--- src/main/java/com/jzo2o/foundations/service/impl/RegionServiceImpl.java
+++ src/main/java/com/jzo2o/foundations/service/impl/RegionServiceImpl.java
@@ -14,13 +14,18 @@ import com.jzo2o.foundations.constants.RedisConstants;
 import com.jzo2o.foundations.enums.FoundationStatusEnum;
 import com.jzo2o.foundations.mapper.CityDirectoryMapper;
 import com.jzo2o.foundations.mapper.RegionMapper;
+import com.jzo2o.foundations.mapper.ServeItemMapper;
+import com.jzo2o.foundations.mapper.ServeMapper;
 import com.jzo2o.foundations.model.domain.CityDirectory;
 import com.jzo2o.foundations.model.domain.Region;
+import com.jzo2o.foundations.model.domain.Serve;
+import com.jzo2o.foundations.model.domain.ServeItem;
 import com.jzo2o.foundations.model.dto.request.RegionPageQueryReqDTO;
 import com.jzo2o.foundations.model.dto.request.RegionUpsertReqDTO;
 import com.jzo2o.foundations.model.dto.response.RegionResDTO;
 import com.jzo2o.foundations.service.IConfigRegionService;
 import com.jzo2o.foundations.service.IRegionService;
+import com.jzo2o.foundations.service.IServeItemService;
 import com.jzo2o.mysql.utils.PageUtils;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -43,7 +48,8 @@ public class RegionServiceImpl extends ServiceImpl<RegionMapper, Region> impleme
     private IConfigRegionService configRegionService;
     @Resource
     private CityDirectoryMapper cityDirectoryMapper;
-
+    @Resource
+    private IServeItemService iServeItemService;
 
     /**
      * 区域新增
@@ -160,7 +166,10 @@ public class RegionServiceImpl extends ServiceImpl<RegionMapper, Region> impleme
             throw new ForbiddenOperationException("草稿或禁用状态方可启用");
         }
         //如果需要启用区域，需要校验该区域下是否有上架的服务
-        //todo
+        Integer count = baseMapper.queryByRegionId(id);
+        if(count == 0){
+            throw new ForbiddenOperationException("该区域下没有上架的服务，不能启用！");
+        }
 
         //更新启用状态
         LambdaUpdateWrapper<Region> updateWrapper = Wrappers.<Region>lambdaUpdate()
@@ -195,11 +204,11 @@ public class RegionServiceImpl extends ServiceImpl<RegionMapper, Region> impleme
         }
 
         //1.如果禁用区域下有上架的服务则无法禁用
-        //todo
-//        int count = serveService.queryServeCountByRegionIdAndSaleStatus(id, FoundationStatusEnum.ENABLE.getStatus());
-//        if (count > 0) {
-//            throw new ForbiddenOperationException("区域下有上架的服务无法禁用");
-//        }
+        //如果需要启用区域，需要校验该区域下是否有上架的服务
+        Integer count = baseMapper.queryByRegionId(id);
+        if(count > 0){
+            throw new ForbiddenOperationException("该区域下有上架的服务，不能禁用！");
+        }
 
         //更新禁用状态
         LambdaUpdateWrapper<Region> updateWrapper = Wrappers.<Region>lambdaUpdate()
